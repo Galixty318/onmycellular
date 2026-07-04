10dade6c37f6249ea4b143cd46dd40d590da43d5
Emily Li <emily@Emilys-Macbook-Pro.local>
2026-07-04 15:08:52 -0700

    Configure GitHub Pages deployment

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -6,7 +6,8 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://galixty318.github.io',
+	base: '/onmycellular',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
